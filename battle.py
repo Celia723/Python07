db333a74f0766da0e70db0857cdde85ad88fe8c0
Celia723 <celiaboyeromar@gmail.com>
2026-06-06 20:01:20 +0200

    ejercicio 0

---
+++ battle.py
@@ -0,0 +1,35 @@
+import ex0.factories as factories
+
+if __name__ == "__main__":
+
+
+    print("Testing factory")
+    flame_factoy = factories.FlameFactory()
+    aqua_factory = factories.AquaFactory()
+
+    Flameling =flame_factoy.create_base()
+    print(Flameling.describe())
+    print(Flameling.attack())
+
+    Pyrodon = aqua_factory.create_evolved()
+    print(Pyrodon.describe())
+    print(Pyrodon.attack())
+
+    print()
+    print("Testing factory")
+    Aquabub = aqua_factory.create_base()
+    print(Aquabub.describe())
+    print(Aquabub.attack())
+
+    Torragon = aqua_factory.create_evolved()
+    print(Torragon.describe())
+    print(Torragon.attack())
+
+    print()
+    print("Testing battle")
+    print(Flameling.describe())
+    print("vs.")
+    print(Aquabub.describe())
+    print("fight!")
+    print(Flameling.attack())
+    print(Aquabub.attack())
---
+++ ex0/factories.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+import ex0.creature as c
+
+class CreatureFactory(ABC):
+    @abstractmethod
+    def create_base(self) -> c.Creature:
+        pass
+    
+    @abstractmethod
+    def create_evolved(self) -> c.Creature:
+        pass
+
+
+class FlameFactory(CreatureFactory):
+    def create_base(self) -> c.Creature:
+        return c.Flameling()
+    
+    def create_evolved(self) -> c.Creature:
+        return c.Pyrodon()
+
+
+class AquaFactory(CreatureFactory):
+    def create_base(self) -> c.Creature:
+        return c.Aquabub()
+    
+    def create_evolved(self) -> c.Creature:
+        return c.Torragon()
+            
+
